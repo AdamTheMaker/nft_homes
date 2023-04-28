e5f95a7ad86f70618a9d938641e00cafed7d018e
AdamTheMaker <adam.bojoh@gmail.com>
2023-04-28 18:30:10 -0400

    listing completed and earnest deposit

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -12,6 +12,22 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "only buyer can call this method");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "only seller can call this method");
+        _;
+    }
+
+    //mapping can be used to hold state values, in this case we are holding states of if the NFT is listed, purchase price, escrow amount, and Buyer
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+
     constructor(
         address _nftAddress,
         address payable _seller,
@@ -24,7 +40,29 @@ contract Escrow {
         lender = _lender;
     }
 
-    function list(uint256 _nftID) public {
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        // Transfer NFT from seller to this contract
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+    }
+
+    //Put Under Contract (only buyer - payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
